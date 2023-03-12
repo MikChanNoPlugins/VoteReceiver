5b227a4b4b997b03479f63a82c6f292f3b2899a4
wtlgo <georgeendo0@gmail.com>
2023-03-12 04:07:18 +0300

    Add IFactory documentation

--- src/main/java/dev/mikchan/mcnp/votereceiver/factory/IFactory.kt
+++ src/main/java/dev/mikchan/mcnp/votereceiver/factory/IFactory.kt
@@ -5,9 +5,37 @@ import dev.mikchan.mcnp.votereceiver.config.IConfig
 import dev.mikchan.mcnp.votereceiver.utility.IUtility
 import io.ktor.server.engine.*
 
+/**
+ * A plugin factory
+ *
+ * Generates essential objects for plugin to function
+ */
 interface IFactory {
+    /**
+     * Creates a configuration object
+     *
+     * @return A configuration object
+     */
     fun createConfig(): IConfig
+
+    /**
+     * Creates a utility object
+     *
+     * @return A utility object
+     */
     fun createUtility(): IUtility
+
+    /**
+     * Creates a web engine
+     *
+     * @return A web engine
+     */
     fun createApplicationEngine(): ApplicationEngine
+
+    /**
+     * Creates a vote handler
+     *
+     * @return A [NuVotifierBukkit] instance
+     */
     fun createVoteHandler(): NuVotifierBukkit?
 }
