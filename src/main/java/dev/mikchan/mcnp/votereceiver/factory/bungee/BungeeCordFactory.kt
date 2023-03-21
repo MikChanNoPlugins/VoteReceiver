6cc72fa004361b6523a49e063e89c8315917e4f1
wtlgo <georgeendo0@gmail.com>
2023-03-21 12:01:00 +0300

    Fix return type

---
+++ src/main/java/dev/mikchan/mcnp/votereceiver/factory/bungee/BungeeCordFactory.kt
@@ -0,0 +1,45 @@
+package dev.mikchan.mcnp.votereceiver.factory.bungee
+
+import com.vexsoftware.votifier.VoteHandler
+import dev.mikchan.mcnp.votereceiver.VoteReceiverBungeeCordPlugin
+import dev.mikchan.mcnp.votereceiver.config.IConfig
+import dev.mikchan.mcnp.votereceiver.config.boosted.BoostedYamlConfig
+import dev.mikchan.mcnp.votereceiver.config.fallback.FallbackConfig
+import dev.mikchan.mcnp.votereceiver.factory.IFactory
+import dev.mikchan.mcnp.votereceiver.utility.IUtility
+import dev.mikchan.mcnp.votereceiver.utility.base.Utility
+import dev.mikchan.mcnp.votereceiver.web.createMineServTopRoute
+import dev.mikchan.mcnp.votereceiver.web.createTMonitoringComRoute
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.routing.*
+import java.io.File
+
+internal class BungeeCordFactory(private val plugin: VoteReceiverBungeeCordPlugin) : IFactory {
+    override fun createConfig(): IConfig {
+        val resource = plugin.getResourceAsStream("config.yml")
+
+        return if (resource != null) {
+            BoostedYamlConfig(File(plugin.dataFolder, "config.yml"), resource)
+        } else {
+            FallbackConfig()
+        }
+    }
+
+    override fun createUtility(): IUtility {
+        return Utility()
+    }
+
+    override fun createApplicationEngine(): ApplicationEngine {
+        return embeddedServer(Netty, port = plugin.config.port) {
+            routing {
+                createMineServTopRoute(plugin)
+                createTMonitoringComRoute(plugin)
+            }
+        }
+    }
+
+    override fun createVoteHandler(): VoteHandler? {
+        return null
+    }
+}
